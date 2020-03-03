8b094b7cc94f221b68a7753b282aa7c498ec262f
vardhan-scripter <saivardhanpoloju326@gmail.com>
2020-03-03 22:59:18 +0530

    Uploading Source Files

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Pixsuit-Assignment</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link href="fonts/ubuntu.css" rel="stylesheet">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <h1 class="header-title">
+                FEATERED APPS
+            </h1>
+        </div>
+        <div class="container row">
+            <div class="col-md-5 col-sm-12 col-xs-12 row handshake-main">
+                <div class="col-sm-4 col-xs-12 handshake-div">
+                    <a href=""></a>
+                    <img src="images/1.png" alt="handshake" class="handshake">
+                    <p>CRM</p>
+                </div>
+                <div class="col-sm-8 col-xs-12 handshake-desc">
+                    <h3>Customer-centric CRM</h3>
+                    <p> Accelerate sales, make smarter decisions, and build better customer relationships.</p>
+                    <button type="button" class="btn">SIGN UP NOW</button>
+                </div>
+            </div>
+            <div class="col-md-7 col-sm-12 col-xs-12">
+                <div class="w-100">
+                    <div class="row">
+                        <div class="col-md-6 col-xs-6" style="padding:0px;">
+                            <div class="card">
+                                <div class="card-body card-body-mobile" style="border-bottom: 1px solid #f8f8f8;border-right: 1px solid #f8f8f8;">
+                                    <a href="#">
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <img src="images/2.png" alt="" class="card-body-img">
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <h4>Books</h4>
+                                                <label>Smart accounting.</label>
+                                                <em style="opacity: 1; visibility: visible;">Sign Up Now</em>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-body card-body-mobile" style="border-right: 1px solid #f8f8f8;">
+                                    <a href="#">
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <img src="images/3.png" alt="" class="card-body-img">
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <h4>Mail</h4>
+                                                <label>Clean and ad-free.</label>
+                                                <em style="opacity: 1; visibility: visible;">Sign Up Now</em>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-xs-6" style="padding:0px;">
+                            <div class="card">
+                                <div class="card-body card-body-mobile" style="border-bottom: 1px solid #f8f8f8;">
+                                    <a href="#">
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <img src="images/4.png" alt="" class="card-body-img">
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <h4>Creator</h4>
+                                                <label>Build apps for your needs.</label>
+                                                <em style="opacity: 1; visibility: visible;">Sign Up Now</em>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-body card-body-mobile">
+                                    <a href="#">
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <img src="images/5.png" alt="" class="card-body-img">
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <h4>Projects</h4>
+                                                <label>Plan and track projects.</label>
+                                                <em style="opacity: 1; visibility: visible;">Sign Up Now</em>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
